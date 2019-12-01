c18c244483d76b45dff7c245aa2e72b00c91f3b5
sb <sb@trint.com>
2019-12-01 10:19:40 +0000

    initial commit

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+// type of token
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLIGAL = "ILLEGAL"
+	EFO     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
